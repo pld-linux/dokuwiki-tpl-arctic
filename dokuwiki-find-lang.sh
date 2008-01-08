e41c6a4f33c8ccd43b0f4253370c4c03bf167fb9
Elan Ruusamäe <glen@pld-linux.org>
2008-01-08 14:00:30 +0000

    - find dokuwiki languages
    Changed files:
        dokuwiki-find-lang.sh -> 1.1

---
+++ dokuwiki-find-lang.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+dir=$RPM_BUILD_ROOT/usr/share/dokuwiki
+langfile=$1
+
+> $langfile
+find $dir -type d -name lang | while read dir; do
+	echo "%dir ${dir#$RPM_BUILD_ROOT}" >> $langfile
+	for dir in $dir/*; do
+		lang=${dir##*/}
+		dir=${dir#$RPM_BUILD_ROOT}
+		case "$lang" in
+		zh-tw)
+			lang=zh_TW
+		;;
+		pt-br)
+			lang=pt_BR
+		;;
+		*-*)
+			: Need mapping for $lang!
+			exit 1
+		;;
+		esac
+		echo "%lang($lang) ${dir#$RPM_BUILD_ROOT}" >> $langfile
+	done
+done
