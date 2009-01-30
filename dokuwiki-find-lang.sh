2420b88365815eebc1c82085d8ce392a1de77b42
Elan Ruusamäe <glen@pld-linux.org>
2009-01-30 14:23:55 +0000

    - do not use subshell, so the exit code could be propagated
    Changed files:
        dokuwiki-find-lang.sh -> 1.5

--- dokuwiki-find-lang.sh
+++ dokuwiki-find-lang.sh
@@ -2,9 +2,13 @@
 PROG=${0##*/}
 dir=$RPM_BUILD_ROOT/usr/share/dokuwiki
 langfile=$1
+tmp=$(mktemp) || exit 1
+rc=0
+
+find $dir -type d -name lang > $tmp
 
 echo '%defattr(644,root,root,755)' > $langfile
-find $dir -type d -name lang | while read dir; do
+while read dir; do
 	echo "%dir ${dir#$RPM_BUILD_ROOT}" >> $langfile
 	for dir in $dir/*; do
 		lang=${dir##*/}
@@ -23,15 +27,18 @@ find $dir -type d -name lang | while read dir; do
 			lang=id_NI
 		;;
 		*-*)
-			echo >&2 "Need mapping for $lang!"
-			exit 1
+			echo >&2 "ERROR: Need mapping for $lang!"
+			rc=1
 		;;
 		esac
 		echo "%lang($lang) ${dir#$RPM_BUILD_ROOT}" >> $langfile
 	done
-done
+done < $tmp
 
 if [ "$(egrep -v '(^%defattr|^$)' $langfile | wc -l)" -le 0 ]; then
 	echo >&2 "$PROG: Error: international files not found!"
-	exit 1
+	rc=1
 fi
+
+rm -f $tmp
+exit $rc
